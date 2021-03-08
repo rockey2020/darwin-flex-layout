e6818b37e2976c8402623ade98ada4fa8a54c7e7
rockey2020 <rockey543400@gmail.com>
2021-03-09 02:14:30 +0800

    优化webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const webpack = require('webpack');
 const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const LICENSE = fs.readFileSync('LICENSE', 'utf8');
 
 //删除没必要的index.js文件
@@ -19,67 +18,69 @@ class removeFiles {
     }
 }
 
-module.exports = {
-    entry: './src/index.js',
-    mode: "production",
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                getLocalIdent: (context, localIdentName, localName, options) => {
-                                    let prefix = "da-flex-";
-                                    let ignorePrefixKey = "_ignore-";
-                                    if ((prefix.slice(0, -1) === localName)) prefix = "";
-                                    if ((localName.includes(ignorePrefixKey))) {
-                                        prefix = "";
-                                        localName = localName.replace(ignorePrefixKey, "");
-                                    }
-                                    return `${prefix}${localName}`;
+module.exports = (env) => {
+    const isProduction = env.NODE_ENV === "production";
+    return {
+        entry: './src/index.js',
+        mode: env.NODE_ENV,
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.less$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    getLocalIdent: (context, localIdentName, localName, options) => {
+                                        let prefix = "da-flex-";
+                                        let ignorePrefixKey = "_ignore-";
+                                        if ((prefix.slice(0, -1) === localName)) prefix = "";
+                                        if ((localName.includes(ignorePrefixKey))) {
+                                            prefix = "";
+                                            localName = localName.replace(ignorePrefixKey, "");
+                                        }
+                                        return `${prefix}${localName}`;
+                                    },
                                 },
-                            },
-                        }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    require('autoprefixer'),
-                                    require('postcss-preset-env'),
-                                    require('postcss-initial'),
-                                    require('postcss-flexbugs-fixes'),
-                                ]
                             }
-                        }
-                    }, 'less-loader'
-                ],
-            }
-        ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'index.min.css'
-        }),
-        new webpack.BannerPlugin({
-            banner: LICENSE
-        }),
-        new removeFiles(),
-    ],
-};
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                postcssOptions: {
+                                    plugins: [
+                                        require('autoprefixer'),
+                                        require('postcss-preset-env'),
+                                        require('postcss-initial'),
+                                        require('postcss-flexbugs-fixes'),
+                                    ]
+                                }
+                            }
+                        }, 'less-loader'
+                    ],
+                }
+            ]
+        },
+        optimization: {
+            minimize: isProduction,
+            minimizer: [
+                new CssMinimizerPlugin(),
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: isProduction ? 'index.min.css' : 'index.css'
+            }),
+            new webpack.BannerPlugin({
+                banner: LICENSE
+            }),
+            new removeFiles(),
+        ],
+    };
+}
